bad6460b58b05fa02b001499d559ff388ee36b4b
Jacob Trock <jtbosshku@gmail.com>
2020-11-15 20:35:54 -0500

    made mobile look better, externalized css, reomved upper/lwr pointers, realigned timer, added click to pause

--- cmp.sh
+++ cmp.sh
@@ -1,1 +1,2 @@
 java -jar ./Compiler/closure-compiler-v20200719.jar --js ns_dev.js --js_output_file ns_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+uglifycss ns.css > ns_prod.css
--- ns_dev.js
+++ ns_dev.js
@@ -68,75 +68,68 @@
             esc();
             outer = output.appendChild(document.createElement('div'));
             inner = outer.appendChild(document.createElement('div'));
-            outer.style.cssText =
-                "position:sticky;left:0px;height:10%;width:100%;background:#f5f5f5;font-size:70px;font-size: 5em;text-align:center;line-height:100px;flex-direction:row;justify-content:center;align-items:center;display:flex;border-top:3px solid;border-bottom:3px solid;";
-            let temp = outer.appendChild(document.createElement('div'));
-            temp.style.cssText =
-                "width:0.18%;background:black;height:8%;position:absolute;top:0%;left:50%;";
-            temp = outer.appendChild(document.createElement('div'));
-            temp.style.cssText =
-                "width:0.18%;background:black;height:8%;position:absolute;bottom:0%;left:50%;";
+            outer.className = "nsp_out";
             pgb = outer.appendChild(document.createElement('input'));
             pgb.type="range";
             pgb.value=pgb.min=0;
             pgb.step=1;
             pgb.max=term.length;
-            pgb.style.cssText =
-                "width:100%;height:4%;position:absolute;bottom:0%;";//TODO:can't make it red
+            pgb.className = "nsp_sli";
             pgb.disabled=true;
             pgb.addEventListener('change', () => {
                 nextWord(pgb.value);
                 inner.innerHTML=term[pgb.value];
             });
             let temp1 = outer.appendChild(document.createElement('div'));
-            temp1.style.cssText =
-                "position:absolute;left:1%;flex-direction:row;display:flex;align-items:center;top:5%;";
-            temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText =
-                "background:#eeeeee;color:black;border:none;font-size:10px;font-size: 0.25em;";
+            temp1.className = "nsp_wdu";
+            
+            let temp = temp1.appendChild(document.createElement('button'));
+            temp.className = "nsp_wb";
             temp.innerHTML = '-';
             temp.addEventListener('click', () => setWpm(-10));
+            
             wpmCounter = temp1.appendChild(document.createElement('input'));
             wpmCounter.type = "text";
             wpmCounter.size = "2";
-            wpmCounter.style.cssText =
-                "font-size:10px;font-size: 0.25em;line-height:10%;height:50%;margin:10px;";
+            wpmCounter.className = "nsp_wad";
             wpmCounter.value = wpm;
             wpmCounter.addEventListener('blur', () => setWpm(wpmCounter.value-wpm));
-            temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText =
-                "background:#eeeeee;color:black;border:none;font-size:10px;font-size: 0.25em;";
-            temp.innerHTML = '+';
-            temp.addEventListener('click', () => setWpm(10));
-            temp1 = outer.appendChild(document.createElement('div'));
-            temp1.style.cssText = "position:absolute;right:1%;flex-direction:row;display:flex;align-items:center;top:5%;";
-            temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText = "background:red;color:black;border:none;font-size:10px;font-size: 0.25em;";
-            temp.innerHTML = '&#9194;&#xFE0E;';
-            temp.addEventListener('click', () => sk(-wpm/4));
             
             temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText = "background:red;color:black;border:none;border-left: 1px solid black;font-size:10px;font-size: 0.25em;";
+            temp.className = "nsp_wb";
+            temp.innerHTML = '+';
+            temp.addEventListener('click', () => setWpm(10));
+            
+            temp1 = outer.appendChild(document.createElement('div'));
+            temp1.className = "nsp_mcl";
+            
+            temp = temp1.appendChild(document.createElement('button'));
+            temp.className = "nsp_pb";
             temp.innerHTML = '&#9199;&#xFE0E;';
             temp.addEventListener('click', () => pause());
             
             temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText =
-                "background:red;color:black;border:none;border-left: 1px solid black;font-size:10px;font-size: 0.25em;";
+            temp.className = "nsp_btn";
+            temp.innerHTML = '&#9194;&#xFE0E;';
+            temp.addEventListener('click', () => sk(-wpm/4));
+                        
+            timect = outer.appendChild(document.createElement('div'));
+            timect.className = "nsp_tmr";
+            timect.innerHTML = "0:00/" + tosec(timeTotal);
+            
+            temp = temp1.appendChild(document.createElement('button'));
+            temp.className = "nsp_btn";
             temp.innerHTML = '&#9193;&#xFE0E;';
             temp.addEventListener('click', () => sk(wpm/4));
                         
             temp = temp1.appendChild(document.createElement('button'));
-            temp.style.cssText =
-                "background:red;color:black;border:none;border-left:1px solid black;font-size:10px;font-size: 0.25em;";
+            temp.className = "nsp_btn";
             temp.innerHTML = '&times;&#xFE0E;';
             temp.addEventListener('click', () => esc());
-            timect = outer.appendChild(document.createElement('div'));
-            timect.style.cssText =
-                "position:absolute;right:1%;flex-direction:row;display:flex;align-items:center;bottom:5%;font-size:10px;font-size: 0.25em;height:15px;";
-            timect.innerHTML = "0:00/" + tosec(timeTotal);
-            inner.style.cssText = "display:inline-block;transform:translate(8%);";
+            
+            inner.className = "nsp_inr";
             inner.innerHTML = 'Press play';
+            inner.addEventListener('click', () => pause());
             outer.onkeydown = (e) => {
             switch (e.keyCode) {
                 case 32:
