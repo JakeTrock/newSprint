3be0098fba476e7d6daeb6d564ef3a826ea993f4
Jacob Trock <jtbosshku@gmail.com>
2020-12-15 20:54:55 -0500

    Update ns_dev.js

--- js/ns_dev.js
+++ js/ns_dev.js
@@ -7,13 +7,12 @@
         let inner;
         let outer;
         let pgb;
-        let pb;
         let wpmCounter;
         let timect;
         let play = false;
         const lCalc = (x) => (1000 / (wpm / 60)) * ((x.length / 5 - 1) * 0.5 + 1);
         const tCalc = (pos=0) => term.slice(pos).reduce((tmp, i) => tmp + lCalc(i), 0);
-        const term = ["--3--", "--2--", "--1--"].concat(
+        const term = (typeof input=="string") ? input.split` `.filter(n => n) : ["--3--", "--2--", "--1--"].concat(
             [].concat.apply([],Array.prototype.slice.call(input).map(r =>
             (r.href) ? r.innerHTML.link(r.href) : (r.innerHTML||r.value).split` `)).filter(n => n));
         let timeTotal = tCalc();
