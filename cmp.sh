42d061aef0838765a0ccce8d942db3baad890097
Jacob Trock <jtbosshku@gmail.com>
2020-12-19 00:09:26 -0500

    switch to npm build sys

--- cmp.sh
+++ cmp.sh
@@ -1,5 +1,5 @@
-java -jar ./Compiler/closure-compiler-v20200719.jar --js ./js/ns_dev.js --js_output_file ./js/ns_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
-java -jar ./Compiler/closure-compiler-v20200719.jar --js ./js/btn_cust.js --js_output_file ./js/cust_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
-java -jar ./Compiler/closure-compiler-v20200719.jar --js ./js/btn_dropin.js --js_output_file ./js/dropin_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+npx google-closure-compiler --js ./js/ns_dev.js --js_output_file ./js/ns_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+npx google-closure-compiler --js ./js/btn_cust.js --js_output_file ./js/cust_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+npx google-closure-compiler --js ./js/btn_dropin.js --js_output_file ./js/dropin_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
 
-uglifycss ./css/ns.css > ./css/ns_prod.css
+npx uglifycss ./css/ns.css > ./css/ns_prod.css
