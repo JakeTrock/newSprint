7bbd921dc5cbf1d6d0b5286561e821d54d52f7e6
Jacob Trock <jtbosshku@gmail.com>
2021-09-02 17:11:48 -0400

    new compilation

--- js/btn_cust.js
+++ js/btn_cust.js
@@ -1,9 +1,20 @@
 (function (d) {
-    var s = d.createElement('script');
-    s.src = 'https://cdn.jsdelivr.net/gh/jaketrock/newSprint@master/js/ns_opt.js';
-    s.type = 'text/javascript';
+  var s = d.createElement("script");
+  s.src = "https://cdn.jsdelivr.net/gh/jaketrock/newSprint@master/js/ns_opt.js";
+  s.type = "text/javascript";
+  function isc(obj = c) {
+    while (obj.tagName.toUpperCase() != "BODY") {
+      if (obj.href == h.href) {
+        return true;
+      }
+      obj = obj.parentNode;
+    }
+    return false;
+  }
+  if (isc()) {
     s.onload = function () {
-        nsprint(d.getElementsByClassName('sprtxt'), d.getElementById('sprFocus'))
+      nsprint(d.getElementsByClassName("sprtxt"), d.getElementById("sprFocus"));
     };
     d.head.appendChild(s);
-}(document));
+  }
+})(document);
--- js/btn_dropin.js
+++ js/btn_dropin.js
@@ -1,16 +1,30 @@
 (function (d) {
-    var s = d.createElement('script'),
-        c = d.createElement('script');
-    c.src = 'https://cdn.jsdelivr.net/gh/jaketrock/rb-c-stage@master/Readable_closure.js';
-    s.src = 'https://cdn.jsdelivr.net/gh/jaketrock/newSprint@master/js/ns_opt.js';
+  var p = "https://cdn.jsdelivr.net/gh/jaketrock/";
+  var t = "text/javascript";
+  var h = d.head;
+  var s = d.createElement("script"),
+    c = d.createElement("script");
+  c.src = p + "rb-c-stage@master/Readable_closure.js";
+  s.src = p + "newSprint@master/js/ns_opt.js";
 
-    s.type = 'text/javascript';
-    c.type = 'text/javascript';
-    s.onload = function() {
-        d.head.appendChild(c)
+  s.type = t;
+  c.type = t;
+
+  function isc() {
+    for (e in h.childNodes) {
+      if (c.href === e.href) {
+        return true;
+      }
+    }
+    return false;
+  }
+  if (isc()) {
+    s.onload = function () {
+      d.head.appendChild(c);
     };
     c.onload = function () {
-        nsprint(new Readability(document.cloneNode(true)).parse().textContent)
+      nsprint(new Readability(document.cloneNode(true)).parse().textContent);
     };
-    d.head.appendChild(s);
-}(document));
+    h.appendChild(s);
+  }
+})(document);
