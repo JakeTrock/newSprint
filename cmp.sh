6ef961e6cedfee035f37c5117544dee59b7b7978
Jacob Trock <jtbosshku@gmail.com>
2021-09-02 20:10:48 -0400

    Update cmp.sh

--- cmp.sh
+++ cmp.sh
@@ -1,5 +1,5 @@
 npx google-closure-compiler --js ./js/ns_dev.js --js_output_file ./js/ns_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
-npx google-closure-compiler --js ./js/btn_cust.js --js_output_file ./js/cust_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
-npx google-closure-compiler --js ./js/btn_dropin.js --js_output_file ./js/dropin_opt.js --compilation_level ADVANCED_OPTIMIZATIONS --rewrite_polyfills --jscomp_off undefinedVars
+npx google-closure-compiler --js ./js/btn_cust.js --js_output_file ./js/cust_opt.js --compilation_level WHITESPACE_ONLY
+npx google-closure-compiler --js ./js/btn_dropin.js --js_output_file ./js/dropin_opt.js --compilation_level WHITESPACE_ONLY
 
 npx uglifycss ./css/ns.css > ./css/ns_prod.css
